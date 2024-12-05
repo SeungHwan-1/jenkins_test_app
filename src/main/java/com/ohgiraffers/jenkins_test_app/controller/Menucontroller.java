b32c896a0ce516447561cebd68262385d7310eff
Lee <cookie3013@naver.com>
2024-12-05 15:20:05 +0900

    dd

--- src/main/java/com/ohgiraffers/jenkins_test_app/controller/Menucontroller.java
+++ src/main/java/com/ohgiraffers/jenkins_test_app/controller/Menucontroller.java
@@ -5,6 +5,7 @@ import com.ohgiraffers.jenkins_test_app.service.Menuservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class Menucontroller {
     @Autowired
     private Menuservice menuservice;
 
+    @CrossOrigin(origins = "http://localhost:3001")
     @GetMapping("/select")
     public ResponseEntity selectAllMenu(){
 
